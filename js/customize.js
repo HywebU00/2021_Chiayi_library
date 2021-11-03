3a88290d54523a39734a16c57f3005b2f94422fd
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2021-11-03 14:44:02 +0800

    新增ＣＳＳ

--- js/customize.js
+++ js/customize.js
@@ -290,3 +290,12 @@ $(function () {
     },
   });
 });
+
+wow = new WOW({
+  animateClass: "animated",
+  offset: 100,
+  callback: function (box) {
+    console.log("WOW: animating <" + box.tagName.toLowerCase() + ">");
+  },
+});
+wow.init();
