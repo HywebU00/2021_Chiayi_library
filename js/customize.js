68d38a985b99ede6f4a4071eaceef7398ec0380d
Your Namemargo-lee <margo.lee@hyweb.com.tw>
2021-11-04 15:10:35 +0800

    新增頁面調整

--- js/customize.js
+++ js/customize.js
@@ -290,12 +290,3 @@ $(function () {
     },
   });
 });
-
-wow = new WOW({
-  animateClass: "animated",
-  offset: 100,
-  callback: function (box) {
-    console.log("WOW: animating <" + box.tagName.toLowerCase() + ">");
-  },
-});
-wow.init();
